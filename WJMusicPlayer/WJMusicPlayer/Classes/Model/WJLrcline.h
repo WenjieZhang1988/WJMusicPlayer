1696d08ef4d47ffab941c9f3a6d97028de587a6d
Kevin <Kevin.1988@foxmail.com>
2015-04-02 23:53:05 +0800

    定义单行歌词模型

---
+++ WJMusicPlayer/WJMusicPlayer/Classes/Model/WJLrcline.h
@@ -0,0 +1,16 @@
+//
+//  WJLrcline.h
+//  WJMusicPlayer
+//
+//  Created by Kevin on 15/4/2.
+//  Copyright (c) 2015年 Kevin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface WJLrcline : NSObject
+
+@property (nonatomic, copy) NSString *time;
+@property (nonatomic, copy) NSString *text;
+
+@end
