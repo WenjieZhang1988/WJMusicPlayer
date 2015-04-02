c43238c9fc839e15351b952612c0849bcd3eb96f
Kevin <Kevin.1988@foxmail.com>
2015-04-02 23:18:56 +0800

    初始化项目

---
+++ WJMusicPlayer/WJMusicPlayer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WJMusicPlayer
+//
+//  Created by Kevin on 15/4/2.
+//  Copyright (c) 2015年 Kevin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WJMusicPlayer/WJMusicPlayer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WJMusicPlayer
+//
+//  Created by Kevin on 15/4/2.
+//  Copyright (c) 2015年 Kevin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
